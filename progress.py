3e8a2e3438e1fce095fcf25dc536ae33fceb3ff3
Drahsid <Drahsid@gmail.com>
2021-07-20 15:15:40 -0400

    Update progress script to output both matching and decompiled %, also add csv support

--- progress.py
+++ progress.py
@@ -8,10 +8,10 @@ import os
 import re
 
 parser = argparse.ArgumentParser(description="Computes current decompilation progress")
-parser.add_argument("-m", "--matching", dest='matching', action='store_true',
-                    help="Output matching progress instead of decompilation progress")
 parser.add_argument("-s", "--silent", dest='silent', action='store_true', help="Run silently")
 parser.add_argument("-u", "--update", dest='update', action='store_true', help="Update the readme")
+parser.add_argument("-d", "--dumpcsv", dest='dumpcsv', action='store_true', help="Append progress data to csv file")
+parser.add_argument("-f", "--csvfile", dest='csvfile', type=str, default="")
 args = parser.parse_args()
 
 NON_MATCHING_PATTERN = r"#ifdef\s+NON_MATCHING.*?#pragma\s+GLOBAL_ASM\s*\(\s*\"(.*?)\"\s*\).*?#endif"
@@ -43,8 +43,6 @@ def GetFiles(path, ext):
 
     return files
 
-nonMatchingFunctions = GetNonMatchingFunctions(GetFiles("src", ".c")) if not args.matching else []
-
 def GetNonMatchingSize(path):
     size = 0
     asmFiles = GetFiles(path, ".s")
@@ -58,6 +56,20 @@ def GetNonMatchingSize(path):
                     size += 4
 
     return size
+
+def UpdateReadme(tofind, pct):
+    with open('README.md', 'r') as file:
+        filedata = file.read()
+
+    index = filedata.find(tofind)
+    if (not index == -1):
+        index += len(tofind)
+        qndex = filedata.find("-", index)
+        if (not qndex == -1):
+            filedata = filedata.replace(filedata[index:qndex], '%.2f' % pct, 1)
+
+    with open('README.md', 'w') as file:
+        file.write(filedata)
 
 
 mapFile = ReadAllLines("build/" + PROGRAM + ".map")
@@ -78,35 +90,53 @@ for line in mapFile:
             elif (objFile.startswith("build/asm")):
                 asm += size
 
-nonMatchingASM = GetNonMatchingSize("asm/nonmatchings")
-
-print("src: " + hex(src) + ", asm: " + hex(asm) + ", nonmatching: " + hex(nonMatchingASM) + ", total: " + hex(src + asm + nonMatchingASM))
-src -= nonMatchingASM
-asm += nonMatchingASM
-
 total = src + asm
-srcPct = 100 * src / total
-asmPct = 100 * asm / total
 
-adjective = "decompiled" if not args.matching else "matched"
+nonMatchingFunctions = []
+nonMatchingSize = GetNonMatchingSize("asm/nonmatchings")
+srcSize = src - nonMatchingSize
+asmSize = asm + nonMatchingSize
+
+
+nonMatchingFunctions = GetNonMatchingFunctions(GetFiles("src", ".c"))
+nonMatchingSize = GetNonMatchingSize("asm/nonmatchings")
+nonMatchingSrcSize = src - nonMatchingSize
+nonMatchingAsmSize = asm + nonMatchingSize
+
+srcPct = 100 * srcSize / total
+asmPct = 100 * asmSize / total
+nonMatchingSrcPct = 100 * nonMatchingSrcSize / total
+nonMatchingAsmPct = 100 * nonMatchingAsmSize / total
+
+hexformat = '0x%06x'
+
+if (args.dumpcsv):
+    git_object = git.Repo().head.object
+    data = [
+        str(git_object.committed_date),
+        git_object.hexsha,
+        str(srcSize), str(asmSize),
+        str(nonMatchingSrcSize), str(nonMatchingAsmSize),
+        str(len(nonMatchingFunctions)),
+        str(total)
+    ]
+
+    if (not args.silent):
+        print(",".join(data))
+
+    dir = args.csvfile
+    if (dir == ""):
+        dir = "progress.csv"
+
+    with open(dir, 'a') as file:
+        file.write("\n" + ",".join(data))
 
 if (not args.silent):
-    print(hex(total) + " total bytes of decompilable code")
-    print("\033[0;32m" + hex(src) + " bytes " + adjective + " in src " + str(srcPct) + "%\033[0;0m")
+    print((hexformat % total).upper() + " total bytes of decompilable code")
+    print("\033[0;32m" + (hexformat % srcSize).upper() + " / " + (hexformat % total).upper() + " matching   (" + str(srcPct) + "%)\033[0;0m")
+    print("\033[0;33m" + (hexformat % nonMatchingSrcSize).upper() + " / " + (hexformat % total).upper() + " decompiled (" + str(nonMatchingSrcPct) + "%)\033[0;0m")
     print("------------------------------------")
 
 if (args.update):
-    with open('README.md', 'r') as file:
-        filedata = file.read()
-
-    tofind = "/" + adjective + "-"
-    index = filedata.find(tofind)
-    if (not index == -1):
-        index += len(tofind)
-        qndex = filedata.find("-", index)
-        if (not qndex == -1):
-            filedata = filedata.replace(filedata[index:qndex], '%.2f' % srcPct, 1)
-
-    with open('README.md', 'w') as file:
-        file.write(filedata)
-
+    UpdateReadme("/matched-", srcPct)
+    UpdateReadme("/decompiled-", nonMatchingSrcPct)
