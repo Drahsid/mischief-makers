57157c3891d36830ea16eb73cc8e2db94c46cef1
blackgamma7 <blackgamma7@gmail.com>
2021-11-30 04:14:44 -0500

    makefile Error

--- src/6F7B30.c
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-//penutimate cutscene actor funcs?
-#pragma GLOBAL_ASM("asm/nonmatchings/6F7B30/func_80192100_6F7B30.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/6F7B30/func_801928E4_6F8314.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/6F7B30/func_80192D08_6F8738.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/6F7B30/func_80193274_6F8CA4.s")
