d66d7f2e19b31c563cc9020f8fb01bacfbbaf197
blackgamma7 <28585957+blackgamma7@users.noreply.github.com>
2026-05-08 19:53:21 -0400

    Update letterbox.h
    typos

--- include/letterbox.h
+++ include/letterbox.h
@@ -5,13 +5,13 @@
 
 //The game uses letterboxing to not only frame out details
 //that may be lost on the overscan of a 4x3 CRT, but also, in select occasions,
-//dyanmically frame the shot between Marina and a boss or other point of interest.
+//dynamically frame the shot between Marina and a boss or other point of interest.
 
 //these should be used with the global value "gLetterboxMode"
 enum LetterboxTypes{
     LETTERBOX_DEFAULT, //standard letterbox for majority of game
-    LETTERBOX_HORIZONTAL, //top and bottom borders get narrower as PoI distance grows
-    LETTERBOX_VERTICAL, //left and right borders get narrower as PoI distance grows
+    LETTERBOX_HORIZONTAL, //top and bottom borders get narrower as x-axis PoI distance grows
+    LETTERBOX_VERTICAL, //left and right borders get narrower as y-axis PoI distance grows
 };
 
 void Gfx_DrawBorderRect(u16 col,s32 x1,s32 y1,s32 x2,s32 y2);
