f9930d616f92e2c04da06560c719047ab502e7a8
blackgamma7 <28585957+blackgamma7@users.noreply.github.com>
2026-07-13 16:08:43 -0400

    add .bss for osd_debug_text

--- src/osd_debug_text.c
+++ src/osd_debug_text.c
@@ -1,5 +1,24 @@
 #include "common.h"
 
+typedef struct {
+    u16 isSet;
+    char text[80];
+    s32 posX;
+    s32 posY;
+    u8 colorR;
+    u8 colorG;
+    u8 colorB;
+    u8 colorA;
+    f32 scaleX;
+    f32 scaleY;
+} OSDStruct;
+
+s32 gOSDDataCount;
+s32 D_80180FD4; // unused.
+OSDStruct gOSDData[40];
+s32 gOSDTime;
+
+
 #pragma GLOBAL_ASM("asm/nonmatchings/osd_debug_text/OSD_Reset.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/osd_debug_text/OSD_SetData.s")
